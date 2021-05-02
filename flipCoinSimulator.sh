ff7d67c922e41d33d672ba4a987891297734f590
unknown <sam1919>
2021-05-02 22:18:08 +0530

    Add: loop through Multiple times of flipping a coin

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -9,3 +9,20 @@ else
         echo "Tails";
 fi
 
+head=0;
+tail=0;
+read -p "Enter number of flip required " n
+for (( i=0; i<n; i++ ))
+do
+	result=$(( RANDOM%2 ));
+	if [ $result == 1 ];
+	then
+         	head=$(($head+1))
+	else
+        	tail=$(($tail+1))
+	fi
+done
+
+echo "Number of head win" $head;
+
+echo "Number of tail win" $tail;
